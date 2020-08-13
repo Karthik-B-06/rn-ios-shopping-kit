e2f9ed047f60f60e8bb366b9e35f402faf01caf2
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-13 15:48:07 +0530

    chore: :bento: Added shop and brand screens images

---
+++ src/utils/assetFiles.js
@@ -0,0 +1,69 @@
+export const shopLight = [
+  "/shop/light/initial.png",
+  "/shop/light/scrolled.png",
+  "/shop/light/searchfocussed.png",
+];
+
+export const shopDark = [
+  "/shop/dark/initial.png",
+  "/shop/dark/scrolled.png",
+  "/shop/dark/searchfocussed.png",
+];
+
+export const brandsMLight = [
+  "/brands-m/light/initial.png",
+  "/brands-m/light/scrolled.png",
+];
+
+export const brandsMDark = [
+  "/brands-m/dark/initial.png",
+  "/brands-m/dark/scrolled.png",
+];
+
+export const brandsDLight = [
+  "/brands-d/light/initial.png",
+  "/brands-d/light/scrolled.png",
+];
+
+export const brandsDDark = [
+  "/brands-d/dark/initial.png",
+  "/brands-d/dark/scrolled.png",
+];
+
+export const discoverLight = [
+  "/discover/light/initial.png",
+  "/discover/light/scrolled.png",
+  "/discover/light/scrolled1.png",
+  "/discover/light/scrolled2.png",
+  "/discover/light/scrolled3.png",
+];
+export const discoverDark = [
+  "/discover/dark/initial.png",
+  "/discover/dark/scrolled.png",
+  "/discover/dark/scrolled1.png",
+  "/discover/dark/scrolled2.png",
+  "/discover/dark/scrolled3.png",
+];
+
+export const accountDark = [
+  "/account/dark/initial.png",
+  "/account/dark/scrolled.png",
+];
+export const accountLight = [
+  "/account/light/initial.png",
+  "/account/light/scrolled.png",
+];
+
+export const checkoutLight = [
+  "/checkout/light/initial.png",
+  "/checkout/light/scrolled.png",
+];
+
+export const checkoutDark = [
+  "/checkout/dark/initial.png",
+  "/checkout/dark/scrolled.png",
+];
+
+export const shoppingbagLight = ["/shoppingbag/light/initial.png"];
+
+export const shoppingbagDark = ["/shoppingbag/dark/initial.png"];
