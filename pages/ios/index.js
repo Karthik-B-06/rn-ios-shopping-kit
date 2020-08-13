e1c14eadc5b14f185167c7e1cadda304efea60b8
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-13 20:04:19 +0530

    style: :sparkles: Ordered the screens based on figma file

--- pages/ios/index.js
+++ pages/ios/index.js
@@ -25,52 +25,70 @@ import {
   wishlistLight,
   wishlistDark,
   collectionLight,
+  collectionDark,
 } from "../../src/utils/assetFiles";
 
 const Ios = (props) => {
   return props?.theme === "LIGHT" ? (
     <>
+      <Sections sections={authLight} sectionTitle="Auth Screen" />
+      <Sections sections={discoverLight} sectionTitle="Discover Screen" />
+      <Sections sections={collectionLight} sectionTitle="Collection Screen" />
+
       <Sections sections={shopLight} sectionTitle="Shop Screen" />
+      <Sections sections={catalogLight} sectionTitle="Catalog Screen" />
+      <Sections sections={designerLight} sectionTitle="Designer Screen" />
+
       <Sections
         sections={brandsMLight}
         sectionTitle="Brands-Manufacturer Screen"
       />
       <Sections
         sections={brandsDLight}
-        sectionTitle="Brands-Designers Screen"
+        sectionTitle="Brands - Designers Screen"
       />
-      <Sections sections={discoverLight} sectionTitle="Discover Screen" />
-      <Sections sections={catalogLight} sectionTitle="Catalog Screen" />
-      <Sections sections={designerLight} sectionTitle="Designer Screen" />
+
       <Sections sections={productLight} sectionTitle="Product Screen" />
+
+      <Sections sections={accountLight} sectionTitle="Account Screen" />
+
       <Sections
         sections={shoppingbagLight}
         sectionTitle="Shopping Bag Screen"
       />
-      <Sections sections={checkoutLight} sectionTitle="Checkout Screen" />
-      <Sections sections={accountLight} sectionTitle="Account Screen" />
-      <Sections sections={authLight} sectionTitle="Auth Screen" />
+
       <Sections sections={wishlistLight} sectionTitle="Wishlist Screen" />
-      <Sections sections={collectionLight} sectionTitle="Collection Screen" />
+
+      <Sections sections={checkoutLight} sectionTitle="Checkout Screen" />
     </>
   ) : (
     <>
+      <Sections sections={authDark} sectionTitle="Auth Screen" />
+      <Sections sections={discoverDark} sectionTitle="Discover Screen" />
+      <Sections sections={collectionDark} sectionTitle="Collection Screen" />
+
       <Sections sections={shopDark} sectionTitle="Shop Screen" />
+      <Sections sections={catalogDark} sectionTitle="Catalog Screen" />
+      <Sections sections={designerDark} sectionTitle="Designer Screen" />
+
       <Sections
         sections={brandsMDark}
         sectionTitle="Brands-Manufacturer Screen"
       />
-      <Sections sections={brandsDDark} sectionTitle="Brands-Designers Screen" />
-      <Sections sections={discoverDark} sectionTitle="Discover Screen" />
-      <Sections sections={catalogDark} sectionTitle="Catalog Screen" />
-      <Sections sections={designerDark} sectionTitle="Designer Screen" />
+      <Sections
+        sections={brandsDDark}
+        sectionTitle="Brands - Designers Screen"
+      />
+
       <Sections sections={productDark} sectionTitle="Product Screen" />
-      <Sections sections={shoppingbagDark} sectionTitle="Shopping Bag Screen" />
-      <Sections sections={checkoutDark} sectionTitle="Checkout Screen" />
+
       <Sections sections={accountDark} sectionTitle="Account Screen" />
-      <Sections sections={authDark} sectionTitle="Auth Screen" />
+
+      <Sections sections={shoppingbagDark} sectionTitle="Shopping Bag Screen" />
+
       <Sections sections={wishlistDark} sectionTitle="Wishlist Screen" />
-      <Sections sections={catalogDark} sectionTitle="Collection Screen" />
+
+      <Sections sections={checkoutDark} sectionTitle="Checkout Screen" />
     </>
   );
 };
