a2bf33cebbdfe77946fe01d2be726081b5972134
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-13 16:05:31 +0530

    feat: :fire: Added Shop and brands sections to iOS

--- pages/ios/index.js
+++ pages/ios/index.js
@@ -1,77 +1,27 @@
-const { default: Sections } = require("../../components/sections");
-
-const shopLight = [
-  './shop/light/initial.png',
-  './shop/light/scrolled.png',
-  './shop/light/withSearch.png',
-]
-const shopDark = [
-  './shop/dark/initial.png',
-  './shop/dark/scrolled.png',
-  './shop/dark/withSearch.png',
-]
-const discoverLight = [
-  './discover/light/initial.png',
-  './discover/light/scrolled.png',
-  './discover/light/scrolled1.png',
-  './discover/light/scrolled2.png',
-  './discover/light/scrolled3.png',
-];
-const discoverDark = [
-  './discover/dark/initial.png',
-  './discover/dark/scrolled.png',
-  './discover/dark/scrolled1.png',
-  './discover/dark/scrolled2.png',
-  './discover/dark/scrolled3.png',
-]
-
-const accountDark = [
-  './account/dark/initial.png',
-  './account/dark/scrolled.png'
-]
-const accountLight = [
-  './account/light/initial.png',
-  './account/light/scrolled.png'
-]
-
-const checkoutLight = [
-  './checkout/light/initial.png',
-  './checkout/light/scrolled.png'
-]
-
-const checkoutDark = [
-  './checkout/dark/initial.png',
-  './checkout/dark/scrolled.png'
-]
-
-const shoppingbagLight = [
-  './shoppingbag/light/initial.png',
-]
-
-const shoppingbagDark = [
-  './shoppingbag/dark/initial.png',
-]
+import {
+  shopLight,
+  shopDark,
+  brandsMLight,
+  brandsMDark,
+  brandsDDark,
+  brandsDLight,
+} from "../../src/utils/assetFiles";
+import Sections from "../../src/components/sections";
 
 const Ios = (props) => {
-  return props?.theme === 'LIGHT' ? (
+  return props?.theme === "LIGHT" ? (
     <>
-      <Sections sections={accountLight} sectionTitle='Account  - Light' />
-      <Sections sections={shopLight} sectionTitle='Shop  - Light' />
-      <Sections sections={discoverLight} sectionTitle='Discover  - Light' />
-      <Sections sections={shoppingbagLight} sectionTitle='Shopping Bag  - Light' />
-      <Sections sections={checkoutLight} sectionTitle='Checkout  - Light' />
-
+      <Sections sections={shopLight} sectionTitle="Shop" />
+      <Sections sections={brandsMLight} sectionTitle="Brands Manufacturer" />
+      <Sections sections={brandsDLight} sectionTitle="Brands Designers" />
     </>
   ) : (
-      <>
-        <Sections sections={accountDark} sectionTitle='Account  - Dark' />
-        <Sections sections={shopDark} sectionTitle='Shop  - Dark' />
-        <Sections sections={discoverDark} sectionTitle='Discover  - Dark' />
-        <Sections sections={shoppingbagDark} sectionTitle='Shopping  - Dark' />
-        <Sections sections={checkoutDark} sectionTitle='Checkout  - Dark' />
-
-      </>
-    )
-}
+    <>
+      <Sections sections={shopDark} sectionTitle="Shop" />
+      <Sections sections={brandsMDark} sectionTitle="Brands Manufacturer" />
+      <Sections sections={brandsDDark} sectionTitle="Brands Designers" />
+    </>
+  );
+};
 
 export default Ios;
